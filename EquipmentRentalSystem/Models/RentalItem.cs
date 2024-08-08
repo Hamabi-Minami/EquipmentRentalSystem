7d1a2f6eb948921dc37b1fec4568c1c80ab238f2
Hamabi_minami <1220625658@qq.com>
2024-08-08 00:35:07 -0600

    new file:   EquipmentRentalSystem/Components/Pages/Customers.razor 	new file:   EquipmentRentalSystem/Models/RentalItem.cs

---
+++ EquipmentRentalSystem/Models/RentalItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentRentalSystem.Models
+{
+    public class RentalItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public Equipment Equipment { get; set; }
+        public int Quantity { get; set; }
+        public double Cost { get; set; }
+
+        [ForeignKey("RentalID")]
+        public int RentalID { get; set; }
+        public Rental Rental { get; set; }
+    }
+}
